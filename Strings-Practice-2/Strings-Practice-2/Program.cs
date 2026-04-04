e908c52f1693311a3c51a46d1575ddd26f0e6884
Adarsh <adarshniharika20@gmail.com>
2026-04-04 14:45:38 +0530

    strings all files added

---
+++ Strings-Practice-2/Strings-Practice-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine().ToLower();
+
+        int count = 0;
+
+        foreach (char ch in input)
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Vowels count: " + count);
+    }
+}
