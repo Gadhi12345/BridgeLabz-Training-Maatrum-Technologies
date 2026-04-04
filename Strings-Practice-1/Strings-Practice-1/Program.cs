95459db3ef0e90cab99c55ceb13062df815a4e4f
Adarsh <adarshniharika20@gmail.com>
2026-04-04 14:44:49 +0530

    Strings added

---
+++ Strings-Practice-1/Strings-Practice-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter size: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Reversed array:");
+        for (int i = n - 1; i >= 0; i--)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
