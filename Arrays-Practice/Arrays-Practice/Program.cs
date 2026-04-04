3c38bf624a3414ab5d25372ccde420d30877aff1
Adarsh <adarshniharika20@gmail.com>
2026-04-04 14:41:26 +0530

    first commit into c# basics

---
+++ Arrays-Practice/Arrays-Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter size: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        Console.WriteLine("Maximum: " + max);
+    }
+}
