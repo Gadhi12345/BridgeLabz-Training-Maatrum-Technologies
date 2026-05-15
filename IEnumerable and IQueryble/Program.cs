e716ea8537f17a8323fe324ac6160b8273c56d4b
Adarsh <adarshniharika20@gmail.com>
2026-05-15 17:31:51 +0530

    LINQ-Ienumerableand IQueryable Updated

---
+++ IEnumerable and IQueryble/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student(){ID = 1, Name = "James", Gender = "Male"},
+                new Student(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+
+
+
+            IQueryable<Student> MethodSyntax = studentList.AsQueryable()
+                               .Where(std => std.Gender == "Male");
+
+            //Iterate through the collection
+            foreach (var student in MethodSyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+
+            /*
+            IEnumerable<Student> QuerySyntax = from std in studentList
+                                               where std.Gender == "Male"
+                                               select std;
+           
+            foreach (var student in QuerySyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+            */
+
+        }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
