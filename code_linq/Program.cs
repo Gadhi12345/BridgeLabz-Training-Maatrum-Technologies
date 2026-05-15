b282eed16a943a4128a0f8bcf41975f00e3f3f66
Adarsh <adarshniharika20@gmail.com>
2026-05-15 12:40:04 +0530

    Code Updated

--- code_linq/Program.cs
+++ code_linq/Program.cs
@@ -4,23 +4,21 @@ using System.Linq;
 class Program { 
     public static void Main(string[] args)
     {
-       List<Student> studentlist = new List<Student>()
-       {
-           new Student(){ID=1,Name="Adarsh",Gender="Male"},
-           new Student(){ID=2,Name="Anwar",Gender="Male"},
-           new Student(){ID=3,Name="Kavya",Gender="Female"},
-       };
-        IQueryable<Student> methodSyntax = studentlist.AsQueryable().Where(std => std.Gender == "Male");
-        foreach(var s in methodSyntax)
+        //Query Syntax
+        List<int> x = new List<int>()
         {
-            Console.WriteLine(s.Name);
+            1,2,3,4,4,5,6,6,7,7,8,8,9
+        };
+
+        var QuerySyntax = from obj in x
+                          where obj > 5
+                          select obj;
+
+        //Method Syntax
+        // var MethodSyntax=x.where(s=>s>5).Tolist();
+        foreach (var t in QuerySyntax)
+        {
+            Console.WriteLine(t);
         }
     }
-}
-
-class Student
-{
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public string Gender { get; set; }
 }
