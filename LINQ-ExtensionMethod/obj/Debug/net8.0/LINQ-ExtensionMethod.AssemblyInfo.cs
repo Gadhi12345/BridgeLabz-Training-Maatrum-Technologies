542cf9894df5a273bb7cac82446217a55d24a32b
Adarsh <adarshniharika20@gmail.com>
2026-05-15 17:36:00 +0530

    LINQ-Extension method Updated

--- LINQ-ExtensionMethod/obj/Debug/net8.0/LINQ-ExtensionMethod.AssemblyInfo.cs
+++ LINQ-ExtensionMethod/obj/Debug/net8.0/LINQ-ExtensionMethod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ-ExtensionMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b282eed16a943a4128a0f8bcf41975f00e3f3f66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3169fb79116af15aac82111978fd2d6f2d7ffaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ-ExtensionMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ-ExtensionMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
